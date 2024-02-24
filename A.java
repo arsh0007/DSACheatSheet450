69cd56177e1498b53a13002919f2426bf7af1dc1
arsh0007 <arshkumar213@gmail.com>
2024-02-24 22:34:03 +0530

    Change in to the file A

--- A.java
+++ A.java
@@ -1,21 +1,26 @@
 import java.util.Scanner;
 
 public class A {
-  public static int [] reverse(int arr[], int n){
+  public static void reverse(int arr[], int n){
+    int reversed [] = new int [n];
     for (int i = 0; i < arr.length; i++) {
-      arr[i]=arr[n-i];
+      reversed[i]=arr[n-i-1];
     }
-    return arr;
+    for (int i : reversed) {
+      System.out.print(i +" ");
+    }
   }
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
-    System.out.println("Enter the size of the array: ");
+    System.out.print("Enter the size of the array: ");
     int n = sc.nextInt();
-    int arr[] = new int[n];
-    System.out.println("Enter the elements in to the array: ");
-    for (int i = 0; i < arr.length; i++) {
-      arr[i]=sc.nextInt();
+    int originalArr[] = new int[n];
+    System.out.print("Enter the elements in to the array: ");
+    for (int i = 0; i < n; i++) {
+      originalArr[i]=sc.nextInt();
     }
-    System.out.println("Reversed arr: "+ reverse(arr, n));
+    System.out.print("[");
+    reverse(originalArr, n);
+    System.out.print("]");
   }
 }
